69a7e9a3bc0810777112217969746b7ed35028c3
Marcos Brasil <marcosvbrasil@yahoo.com.br>
2023-04-22 14:05:54 -0300

    Implementing IValueConverter

---
+++ WeatherApp/ViewModel/ValueConverters/BoolToRainConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WeatherApp.ViewModel.ValueConverters
+{
+    public class BoolToRainConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isRaining = (bool)value;
+
+            if(isRaining)
+            {
+                return "Currently raining";
+            }
+            else
+            {
+                return "Currently not raining";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string isRaining = (string)value;
+
+            if (isRaining == "Currently raining")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
