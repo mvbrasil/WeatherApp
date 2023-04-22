ab3845af99e83a478f3b165b9737d1b5f08b30c9
Marcos Brasil <marcosvbrasil@yahoo.com.br>
2023-04-21 23:16:30 -0300

    implementing classes

---
+++ WeatherApp/ViewModel/WeatherVM.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Model;
+using WeatherApp.ViewModel.Commands;
+using WeatherApp.ViewModel.Helpers;
+
+namespace WeatherApp.ViewModel
+{
+    public class WeatherVM : INotifyPropertyChanged
+    {
+        private string query;
+            
+        public string Query
+        {
+            get { return query; }
+            set 
+            {
+                query = value;
+                OnPropertyChanged("Query");
+            }
+        }
+
+        private CurrentConditions currentConditions;
+
+        public CurrentConditions CurrentConditions
+        {
+            get { return currentConditions; }
+            set
+            { 
+                currentConditions = value;
+                OnPropertyChanged("CurrentConditions");
+            }
+        }
+
+        private City selectedCity;
+
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set { selectedCity = value; }
+        }
+
+        public SearchCommand SearchCommand { get; set; }
+
+
+        public WeatherVM()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            {
+                SelectedCity = new City
+                {
+                    LocalizedName = "New York"
+                };
+
+                CurrentConditions = new CurrentConditions
+                {
+                    WeatherText = "Partly cloudy",
+                    Temperature = new Temperature
+                    {
+                        Metric = new Units
+                        {
+                            Value = 21
+                        }
+                    }
+                };
+            }
+
+            SearchCommand = new SearchCommand(this);
+        }
+
+        public async void MakeQuery()
+        {
+            var cities = await AccuWeatherHelper.GetCities(Query);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged (string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
